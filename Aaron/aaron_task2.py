629a9ee495e313e55e5e869006378368af935732
Aaron Lim <36909673+darronlim@users.noreply.github.com>
2018-03-20 09:29:27 +1000

    Task2 Update

--- Aaron/aaron_task2.py
+++ Aaron/aaron_task2.py
@@ -5,16 +5,35 @@ Created on Tue Mar 13 14:45:03 2018
 
 @author: aaron
 """
+import os
+
+os.getcwd()
+
+import numpy as np
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 
-# preprocessing
-org1 = pd.read_csv('organics.csv')
+def data_prep():
+    
+    # read the organics dataset 
+    org1 = pd.read_csv('organics.csv')
 
-# one-hot encoding
-org1 = pd.get_dummies(org1)
+    # drop unnecesary variables
+    org1.drop(['CUSTID', 'DOB'], axis=1, inplace=True)
+    
+    # remove data with NaN
+    org1 = org1.dropna(axis = 0, how='any')
+    
+    # one-hot encoding
+    org1 = pd.get_dummies(org1)
 
-# display info
-print(org1.info())
+    print(org1.info())
+    
+    return org1
+
+# call data_prep method
+org1 = data_prep()
 
 # target/input split
 y = org1['ORGANICS']
@@ -34,3 +53,96 @@ model = DecisionTreeClassifier(random_state=rs)
 model.fit(X_train, y_train)
 
 print("Train accuracy:", model.score(X_train, y_train))
+print("Test accuracy:", model.score(X_test, y_test))
+
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+
+# Check feature importance
+import numpy as np
+
+# grab feature importances from the model and feature name from the original X
+importances = model.feature_importances_
+feature_names = X.columns
+
+#sort them out in descending order
+indices = np.argsort(importances)
+indices = np.flip(indices, axis=0)
+
+#limit to 20 features
+indices = indices[:20]
+
+for i in indices:
+    print(feature_names[i], ":", importances[i])
+    
+# Visualising
+import pydot
+from io import StringIO
+from sklearn.tree import export_graphviz
+
+dotfile = StringIO()
+export_graphviz(model, out_file = dotfile, feature_names = X.columns)
+graph = pydot.graph_from_dot_data(dotfile.getvalue())
+graph[0].write_png("aarontask2_org_vis.png")
+
+
+# hyperparameters and model performance
+test_score = []
+train_score = []
+
+# check model performance for max depth from 2-20
+for max_depth in range(2, 21):
+    model = DecisionTreeClassifier(max_depth=max_depth, random_state=rs)
+    model.fit(X_train, y_train)
+    
+    test_score.append(model.score(X_test, y_test))
+    train_score.append(model.score(X_train, y_train))
+    
+import matplotlib.pyplot as plt
+
+# plot max depth hyperparameter values vs training and test accuracy score
+plt.plot(range(2, 21), train_score, 'b', range(2,21), test_score, 'r')
+plt.xlabel('max_depth\nBlue = Training Acc. Red = Test Acc.')
+plt.ylabel('accuracy')
+plt.show()
+
+
+# find optimal hyperparamters with GridSearchCV
+from sklearn.model_selection import GridSearchCV
+
+#grid search CV
+params = {'criterion': ['gini', 'entropy'],
+          'max_depth': range(2,7),
+          'min_samples_leaf': range(20,60,10)}
+
+cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(random_state=rs), cv=10)
+cv.fit(X_train, y_train)
+
+print("Train accuracy:", cv.score(X_train, y_train))
+print("Test accuracy:", cv.score(X_test, y_test))
+
+#test the best model
+y_pred = cv.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+#print parameters of the best model
+print(cv.best_params_)
+
+#grid search CV #2
+params = {'criterion': ['gini', 'entropy'],
+          'max_depth': range(2,6),
+          'min_samples_leaf': range(45,56)}
+
+cv = GridSearchCV(param_grid=params, estimator=DecisionTreeClassifier(random_state=rs), cv=10)
+cv.fit(X_train, y_train)
+
+print("Train accuracy:", cv.score(X_train, y_train))
+print("Test accuracy:", cv.score(X_test, y_test))
+
+#test the best model
+y_pred = cv.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+#print parameters of the best model
+print(cv.best_params_)
